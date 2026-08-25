4c2891e7170e94af8d8e838357ba5f1d50d1fb01
Aurélie Gangloff <aurelie@sdv.fr>
2026-08-25 10:33:39 +0200

    add module modale

--- app/components/ContactButton/ContactButton.jsx
+++ app/components/ContactButton/ContactButton.jsx
@@ -1,4 +1,5 @@
 "use client";
+import Modal from "../Modal/Modal";
 import styles from "./ContactButton.module.css";
 import { useState } from "react";
 
@@ -15,52 +16,7 @@ export default function ContactButton({ children, photographer }) {
       </button>
 
       {isModalOpen && (
-        <div
-          className={styles.modal}
-          role="dialog"
-          aria-modal="true"
-          aria-labelledby="modalTitle"
-        >
-          <div className={styles.modalContent}>
-            <header className={styles.modalHeader}>
-              <h1 id="modalTitle" className={styles.modalTitle}>
-                Contactez-moi
-                <br />
-                {photographer.name}
-              </h1>
-              <button
-                type="button"
-                className={styles.modalCloseButton}
-                onClick={() => setIsModalOpen(false)}
-                aria-label="Close Contact form"
-              ></button>
-            </header>
-            <form
-              className={styles.contactForm}
-              onSubmit={(event) => event.preventDefault()}
-            >
-              <label htmlFor="firstName">Prénom</label>
-              <input id="firstName" name="firstName" type="text" required />
-
-              <label htmlFor="lastName">Nom</label>
-              <input id="lastName" name="lastName" type="text" required />
-
-              <label htmlFor="email">Email</label>
-              <input id="email" name="email" type="email" required />
-
-              <label htmlFor="message">Votre message</label>
-              <textarea id="message" name="message" rows="5" required />
-
-              <button
-                type="submit"
-                className={styles.formSubmit}
-                aria-label="Send"
-              >
-                Envoyer
-              </button>
-            </form>
-          </div>
-        </div>
+        <Modal photographer={photographer} setIsModalOpen={setIsModalOpen} />
       )}
     </>
   );
---
+++ app/components/Modal/Modal.jsx
@@ -0,0 +1,48 @@
+import styles from "./Modale.module.css";
+
+export default function Modal({ photographer, setIsModalOpen }) {
+  return (
+    <div
+      className={styles.modal}
+      role="dialog"
+      aria-modal="true"
+      aria-labelledby="modalTitle"
+    >
+      <div className={styles.modalContent}>
+        <header className={styles.modalHeader}>
+          <h1 id="modalTitle" className={styles.modalTitle}>
+            Contactez-moi
+            <br />
+            {photographer.name}
+          </h1>
+          <button
+            type="button"
+            className={styles.modalCloseButton}
+            onClick={() => setIsModalOpen(false)}
+            aria-label="Close Contact form"
+          ></button>
+        </header>
+        <form
+          className={styles.contactForm}
+          onSubmit={(event) => event.preventDefault()}
+        >
+          <label htmlFor="firstName">Prénom</label>
+          <input id="firstName" name="firstName" type="text" required />
+
+          <label htmlFor="lastName">Nom</label>
+          <input id="lastName" name="lastName" type="text" required />
+
+          <label htmlFor="email">Email</label>
+          <input id="email" name="email" type="email" required />
+
+          <label htmlFor="message">Votre message</label>
+          <textarea id="message" name="message" rows="5" required />
+
+          <button type="submit" className={styles.formSubmit} aria-label="Send">
+            Envoyer
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
